1640e017da543ee7f97d774440611d7016dad006
codeninjascollierville <107887805+codeninjascollierville@users.noreply.github.com>
2023-03-15 18:46:25 -0500

    erw gtg
    6 h5ht

---
+++ Assets/Scripts/RocketAnimation.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketAnimation : MonoBehaviour
+{
+
+    [Header("Animator")]
+    public Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("HAxis", horizontal);
+    }
+}
